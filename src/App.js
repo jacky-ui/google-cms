26b1e43c94c11d23a0aadfd663aeb40472469f6c
jacky-ui <jacky.du.004@gmail.com>
2024-05-17 14:04:09 -0400

    import and set state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './styles/App.css';
 import { Axios } from 'axios';
+import { useState, useEffect } from 'react';
 const { GOOGLE_WEBSITE } = process.env;
 
 function App() {
+  const [cmsContent, setCmsContent] = useState(null);
+
   return (
     <>
       <h1>Test using Google Sheets as CMS</h1>
