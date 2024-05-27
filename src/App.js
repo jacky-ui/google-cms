2e455827bfc75ac23e919ab020109254fad29661
jacky-ui <jacky.du.004@gmail.com>
2024-05-27 19:21:09 -0400

    able to map data from google sheet

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
     axios
       .get(REACT_APP_URL)
       .then((response) => {
-        console.log(response.data.data);
+        console.log(response.data);
+        console.log(typeof([response.data]))
         setCmsContent(response.data.data);
       })
       .catch((error) => {
@@ -25,12 +26,20 @@ function App() {
       {cmsContent ? 
         <div>
           <h1>Test using Google Sheets as CMS</h1>
-          <ul>
-            <li>
-              <h2></h2>
-              <p></p>
-            </li>              
-          </ul>
+            <div className='container'>
+              {cmsContent.map((contents) => (
+                <ul key={contents.title}>
+                  <li>
+                    <h2>{contents.title}</h2>
+                    <img 
+                      src={contents.image}
+                      alt=""
+                    />
+                    <p>{contents.contents}</p>
+                  </li>
+                </ul>
+              ))}
+            </div>
         </div>
         :
         <h1>{errorMessage}</h1>
